27ab8158a534b0b9d80ab0a765abf69382e9116d
Josh Mandel <jmandel@med.harvard.edu>
2019-09-06 11:55:08 -0500

    Updates

---
+++ precache-manifest.2b96313a8801ba55bc754fef84cff83f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e703e1fb1aab944c59d98c80b2a4ef",
+    "url": "/pama-demo-app/index.html"
+  },
+  {
+    "revision": "e0560ec995d5844176c0",
+    "url": "/pama-demo-app/static/css/main.fe63bf67.chunk.css"
+  },
+  {
+    "revision": "0bfcae7fa3bad5d4a535",
+    "url": "/pama-demo-app/static/js/2.f5b28ab3.chunk.js"
+  },
+  {
+    "revision": "e0560ec995d5844176c0",
+    "url": "/pama-demo-app/static/js/main.5ee8f671.chunk.js"
+  },
+  {
+    "revision": "2cc71f126fae6c236dae",
+    "url": "/pama-demo-app/static/js/runtime~main.63629d37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b2d28612b0d99ee693653cc783aa85c.js"
+  "/pama-demo-app/precache-manifest.2b96313a8801ba55bc754fef84cff83f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pama-demo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
