aaa58bbdeb57f2547ffbd2300381602e9c249a43
Dennis Patterson <dennis.r.patterson@gmail.com>
2019-09-14 17:53:09 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pama-demo-app/precache-manifest.1bec3f811d7c1921a0f896d91f5b57b3.js"
+  "/cds-hooks/pama-demo-app/precache-manifest.7ebd9cea11a6b0f50ebabcdbc8451b3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pama-demo-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cds-hooks/pama-demo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
